24d137f7e2825c1505805907b86bb7d170af94b3
sagiassa <51288247+sagiassa@users.noreply.github.com>
2019-11-18 23:19:40 +0200

    main search page design is finished

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -19,15 +19,13 @@ class SearchResults extends Component {
         let post = this.props.post
         return (
             <div className="postDisplay" >
-                <div className="title" > {post.title} </div>
+                <div className="Title" > {post.title} </div>
                 <div className="image">
                     {post.type === "Animated" ?
                         <video controls src={post.images.image460sv.url} type="video/mp4" />
                         : <img src={post.images.image460.url} />}
                 </div>
-
-                <div className="type"> {post.type} </div>
-                <div className="details" > <Link to='/DetailsPage' onClick={this.GetMoreDetails}>Get More Details here</Link> </div>
+                <div className="details" > <Link to='/DetailsPage' onClick={this.GetMoreDetails} >Get More Details</Link> </div>
             </div>)
     }
 }
